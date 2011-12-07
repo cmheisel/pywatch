f68a01ec8f7a7bd1be3656648185688486f14c19
Alfredo Deza <alfredodeza@gmail.com>
2011-12-06 19:21:36 -0500

    add command line option for clearing terminal

--- src/pywatch/__init__.py
+++ src/pywatch/__init__.py
@@ -27,6 +27,10 @@ def main(args=None):
                       dest="version",
                       default=False,
                       help="Output verion number and exit.")
+    parser.add_option("--no-clear",
+                      action="store_true",
+                      default=False,
+                      help="Don't clear the terminal when files change.")
     options, args = parser.parse_args(args)
 
     if options.version:
@@ -38,6 +42,6 @@ def main(args=None):
 
     cmds = [args[0], ]
     files = args[1:]
-    w = Watcher(cmds=cmds, files=files, verbose=options.verbose)
+    w = Watcher(cmds=cmds, files=files, verbose=options.verbose, clear=options.no_clear)
     w.run_monitor()
     sys.exit(0)
